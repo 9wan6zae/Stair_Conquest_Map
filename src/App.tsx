ffbed9a76870d4ef6dfaf33b6aa4d3268c85ee6d
한광재 <hkj4900@pusan.ac.kr>
2021-07-18 20:17:42 +0900

    feat: 라우터 돔을 이용해 페이지 이동 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './css/Font.css'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import AppHeader from './components/appHeader';
+import mainPage from './pages/mainPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <AppHeader></AppHeader>
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path="/" component={mainPage} />
+          {/* Not Found */}
+          <Route component={() => <Redirect to="/" />} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
