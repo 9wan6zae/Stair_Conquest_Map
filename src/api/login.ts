1d112ba0b2cbd83b9ef88b9c5328cbb05cc0566a
한광재 <hkj4900@pusan.ac.kr>
2021-08-26 22:43:28 +0900

    feat: 회원가입을 위한 API 등록

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,9 +1,14 @@
 import axios from "../utils/axios";
 import { AxiosResponse } from "axios";
+import { SignUpParams } from "../types/Sign";
 
 export function login(nickname: string, password: string): Promise<AxiosResponse> {
   return axios.post("/login", {
     nickname,
     password,
   });
+}
+
+export function signUp (params: SignUpParams): Promise<AxiosResponse> {
+  return axios.post("/signUp", params);
 }
