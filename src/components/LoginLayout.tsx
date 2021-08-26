902a238bf6cf43690c7524ce4a6ea81b4b799ec4
한광재 <hkj4900@pusan.ac.kr>
2021-08-26 22:43:46 +0900

    feat: 로그인과 회원가입에 적용되는 레이아웃 개발

---
+++ src/components/LoginLayout.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import AppHeader from '../components/appHeader';
+import styled from 'styled-components';
+
+const MainBlock = styled.main`
+  position: relative;
+  width: 100%;
+  max-width: var(--maxWidth);
+  padding: 0 20px;
+  box-sizing: border-box;
+
+  .login__title_section {
+    margin-top: 36px;
+    margin-bottom: 32px;
+    text-align: center;
+  }
+
+  .login__title {
+    margin-bottom: 12px;
+  }
+
+  .login__input-box {
+    margin-bottom: 16px;
+  }
+`
+
+type LayoutProps = {
+  title: string
+  description: string
+  content: React.ReactElement
+  footer: React.ReactElement
+}
+
+export default function LoginLayout({title, description, content, footer}: LayoutProps) {
+  return (
+    <>
+      <AppHeader title={title}></AppHeader>
+      <MainBlock>
+        <section className="login__title_section">
+          <p className="title4 login__title">계단정복지도</p>
+          <p className="description">{description}</p>
+        </section>
+        {content}
+      </MainBlock>
+      {footer}
+    </>
+  )
+}
