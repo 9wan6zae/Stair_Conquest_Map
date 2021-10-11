9af4e25e61c74de2a1408b0f1ed260a829562cca
한광재 <hkj4900@pusan.ac.kr>
2021-10-11 21:40:50 +0900

    fix: 계단 / 경사로 링크 수정

--- src/ts/link.ts
+++ src/ts/link.ts
@@ -1,6 +1,6 @@
 export const link: link_type = {
-  계단: "https://www.notion.so/eggnrock/8312cc653a8f4b9aa8bc920bbd668218",
-  경사로: "https://www.notion.so/eggnrock/6f64035a062f41e28745faa4e7bd0770"
+  계단: "https://eggnrock.notion.site/8312cc653a8f4b9aa8bc920bbd668218",
+  경사로: "https://eggnrock.notion.site/6f64035a062f41e28745faa4e7bd0770"
 }
 
 interface link_type {
