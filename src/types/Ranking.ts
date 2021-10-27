e9c3993f71f0b35c9d09ed7d083ee8b18ff944f3
한광재 <hkj4900@pusan.ac.kr>
2021-10-27 23:05:24 +0900

    chore: 불필요한 타입 제거

--- src/types/Ranking.ts
+++
@@ -1,9 +0,0 @@
-import { Village } from "./Model"; 
-
-export interface VillageRankingEntry {
-  village: Village | undefined;
-  /** 계단정복률 순위. */
-  progressRank: number;
-  /** 계단정복률. */
-  progressPercentage: string;
-}
