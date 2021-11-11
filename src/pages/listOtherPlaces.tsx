0dc18b84581a56f5d0561f9fa40dad7fdcad64ef
한광재 <hkj4900@pusan.ac.kr>
2021-11-11 22:06:34 +0900

    feat: listOtherPlacesPage 구현

--- src/pages/listOtherPlaces.tsx
+++ src/pages/listOtherPlaces.tsx
@@ -1,11 +1,47 @@
 import * as React from 'react';
+import * as ListOtherPlacesAPI from '../api/listOtherPlaces'
+import MainHeader from '../components/mainHeader';
+import { SearchPlacesResult, SearchPlacesResult_Item } from '../types/SearchPlaces';
+import { ListPlaces } from './searchPage';
+
+import { useDispatch } from 'react-redux';
+import { set_item } from '../modules/item';
 
 export default function ListOtherPlaces({location}: {location: any}) {
+  const dispatch = useDispatch();
+  const [places, setPlaces] = React.useState<SearchPlacesResult>({items: []})
   React.useEffect(() => {
     const buildingId = location.state.buildingId
-    console.log(buildingId)
+    ListOtherPlacesAPI.listPlacesInBuilding({buildingId}).then((res) => setPlaces(res.data))
   }, [location.state.buildingId])
+
+  const [selectItem, setSelectItem] = React.useState<SearchPlacesResult_Item>(
+    {
+      place: {
+        id: '',
+        name: '',
+        address: ''
+      },
+      building: {
+        id: '',
+        address: ''
+      },
+      hasBuildingAccessibility: false,
+      hasPlaceAccessibility: false,
+      distanceMeters: undefined
+    },
+  );
+
+  const setItem = (item: SearchPlacesResult_Item) => {
+    setSelectItem(item)
+    dispatch(set_item(item));
+  }
   return (
-    <></>
+    <>
+      <MainHeader />
+      <section style={{marginTop: '20px'}}>
+        <ListPlaces places={places} selectItem={selectItem} setItem={setItem}  />
+      </section>
+    </>
   )
 }
